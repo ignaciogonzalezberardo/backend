0cb7d8869e1bc3fa77f902c1afe93bfaf6a6b8c0
Nacho <149008002+ignaciogonzalezberardo@users.noreply.github.com>
2024-11-15 20:55:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const cors=require('cors')
+const UserRoutes=require('./routes/user.routes')
+const productRoutes=require('./routes/product.routes')
+const categoryRoutes=require('./routes/category.routes')
+const orderRoutes=require('./routes/order.routes')
+app.use(cors())
+app.use(express.static('public'))
+app.use( express.json())
+app.use([UserRoutes, productRoutes,categoryRoutes,orderRoutes])
+module.exports=app
